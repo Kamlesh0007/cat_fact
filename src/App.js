0689d0c35ad7f94d7ee962b248cd111d16eb4ef5
Kamlesh0007 <wwwkamleshyadav732@gmail.com>
2023-05-12 21:02:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
+import refreshIcon from './assets/refresh.png'
 
 function App() {
+  const [fact, setFact] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+
+
+  const fetchFact = () => {
+    setLoading(true);
+    axios.get("https://catfact.ninja/fact").then((res) => {
+      setFact(res.data.fact);
+      setLoading(false);
+    });
+  };
+  
+  const handleToggle = () => {
+    setShowMore(!showMore);
+  };
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
+  const handleRefresh = () => {
+    fetchFact();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Cat Fact</h1>
+      {loading ? 
+      (<div className="loader">
+        <div className="dot-loader"></div>
+        <div className="dot-loader dot-loader--2"></div>
+        <div className="dot-loader dot-loader--3"></div>
+      </div>):(<p>"{fact}"</p>)
+      
+      }
+      <button onClick={handleRefresh}><img src={refreshIcon} alt="refresh button" /></button>
     </div>
   );
 }
